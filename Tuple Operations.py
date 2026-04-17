fb031188fcaed87ba7176c8a1da8fb7b171337df
harshanjaswanth-oss <Jaswanth902>
2026-04-17 16:34:45 +0530

    tuple operations

---
+++ Tuple Operations.py
@@ -0,0 +1,9 @@
+tuple1=("harshan",12,91.2,True)
+tuple2=(190,32,65,73,67,65)
+tuple3=tuple2+(9, )
+
+print(tuple3)
+
+print(tuple2.count(65))
+
+print(tuple1[2:4])
---
+++ average.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+sum=0
+marks=[82,98,78,99,82,100]
+for i in marks:
+
+    sum=sum+i
+average=sum/len(marks)
+print(average)             
+
+print(max(marks))
+print(min(marks))
+
+marks.append(90)
+print(marks)
+
+marks.sort()
+print(marks)
+
+
+        
