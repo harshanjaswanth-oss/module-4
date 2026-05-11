ddae89c8a837d341a0fdeeed234f49b52e5f1827
harshanjaswanth-oss <Jaswanth902>
2026-05-11 14:31:21 +0530

    Convert degrees to radians

---
+++ trignometric calculator.py
@@ -0,0 +1,11 @@
+import math
+
+angle_deg = float(input("Enter angle in degrees: "))
+
+
+angle_rad = math.radians(angle_deg)
+
+
+print("sin =", math.sin(angle_rad))
+print("cos =", math.cos(angle_rad))
+print("tan =", math.tan(angle_rad))
